807ffef7d01719bd7848e6b7599488fbbaf4a56d
kamal singh <kamalsinghbs347@gmail.com>
2025-11-29 12:34:33 +0530

    Added the code for selection sort

---
+++ selectionSort.java
@@ -0,0 +1,28 @@
+
+import java.util.Arrays;
+
+public class selectionSort {
+
+    public static void selectionSortAlgo(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {12, 1, 10, 9, 15, 22};
+        selectionSortAlgo(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
