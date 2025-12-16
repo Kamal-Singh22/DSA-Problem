1d2e90ba37e36f1a8515a9ab2c5ab0e87cf70fc7
kamal singh <kamalsinghbs347@gmail.com>
2025-12-16 23:25:41 +0530

    add

---
+++ oneOcc.java
@@ -0,0 +1,17 @@
+
+class oneOcc {
+
+    public static int singleOccurance(int arr[]) {
+        int n = arr.length;
+        int xor = 0;
+        for (int i = 0; i < n; i++) {
+            xor = xor ^ arr[i];
+        }
+        return xor;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 1, 2, 2, 3, 3, 4, 4, 5};
+        System.out.println(singleOccurance(arr));
+    }
+}
