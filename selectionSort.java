b28a09350c3a69f6827e69139d4a849b70f33dd3
kamal singh <kamalsinghbs347@gmail.com>
2026-01-08 23:05:37 +0530

    update

--- selectionSort.java
+++ selectionSort.java
@@ -1,33 +1,35 @@
 
-// import java.util.Arrays;
-// public class selectionSort {
-//     public static void selectionSortAlgo(int arr[]) {
-//         int n = arr.length;
-//         for (int i = 0; i < n - 1; i++) {
-//             int min = i;
-//             for (int j = i + 1; j < n; j++) {
-//                 if (arr[j] < arr[min]) {
-//                     min = j;
-//                 }
-//             }
-//             int temp = arr[i];
-//             arr[i] = arr[min];
-//             arr[min] = temp;
-//         }
-//     }
-//     public static void main(String[] args) {
-//         int arr[] = {12, 1, 10, 9, 15, 22};
-//         selectionSortAlgo(arr);
-//         System.out.println(Arrays.toString(arr));
-//     }
-// }
 import java.util.Arrays;
 
-class Main {
+public class selectionSort {
+
+    public static void selectionSortAlgo(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
 
     public static void main(String[] args) {
-        int[] arr = {12, 1, 10, 9, 15, 22};
-        Arrays.sort(arr);
+        int arr[] = {12, 1, 10, 9, 15, 22};
+        selectionSortAlgo(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
+
+// import java.util.Arrays;
+// class Main {
+//     public static void main(String[] args) {
+//         int[] arr = {12, 1, 10, 9, 15, 22};
+//         Arrays.sort(arr);
+//         System.out.println(Arrays.toString(arr));
+//     }
+// }
